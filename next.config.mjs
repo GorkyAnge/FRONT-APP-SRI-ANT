deb55a2ee2548600afd8ae6d775f394353d24374
GorkyAnge <gorkyange@gmail.com>
2024-05-27 21:55:43 -0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/puntos/:path*',
+          destination: 'http://localhost:8081/puntos/:path*', // Proxy to localhost:8081
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
